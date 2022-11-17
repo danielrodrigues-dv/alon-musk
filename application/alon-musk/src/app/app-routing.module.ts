ebaf8865673b0558ccbde7ffe962071fae02df8d
Daniel Rodrigues <41621213+danielrodrigues-dv@users.noreply.github.com>
2022-11-17 17:23:20 -0300

    Add files via upload

---
+++ application/alon-musk/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './pages/error/error.component';
+import { HomeComponent } from './pages/home/home.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
