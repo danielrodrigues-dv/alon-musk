a4888ba1c4ac4becdd438597b93852fa1a3247c2
Daniel Rodrigues <41621213+danielrodrigues-dv@users.noreply.github.com>
2022-11-17 18:21:47 -0300

    Add files via upload

---
+++ application/src/app/pages/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css'],
+})
+export class ErrorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+
+}
